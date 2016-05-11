af0e53ec2d8e192f229287d1304d479d15db59ac
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-11 15:27:59 +0200

    Install script placeholder

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
 }
 
 require_once (#theme_namespace_caps#)_PATH . 'library/Vendor/Psr4ClassLoader.php';
-$loader = new Dunkers\Vendor\Psr4ClassLoader();
+$loader = new (#theme_namespace_caps#)\Vendor\Psr4ClassLoader();
 $loader->addPrefix('(#theme_namespace#)', (#theme_namespace_caps#)_PATH . 'library');
 $loader->addPrefix('(#theme_namespace#)', (#theme_namespace_caps#)_PATH . 'source/php/');
 $loader->register();
